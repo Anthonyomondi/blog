4be154cc9c86d36c1a943dfecc56e132448c0409
Anthonyomondi <otieno.anthonyomondi00@gmail.com>
2021-04-09 12:11:17 +0300

    Ready for deployment to heroku.

--- tony_blog/settings.py
+++ tony_blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,5 +135,4 @@ STATIC_URL = '/static/'
 LOGIN_URL = 'users:login'
 
 # Heroku settings.
-import django_heroku
 django_heroku.settings(locals())
