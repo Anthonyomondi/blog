bdf6246b4c5fc1465801fdaf9503f61ec45d26f5
Anthonyomondi <otieno.anthonyomondi00@gmail.com>
2021-04-12 20:40:59 +0300

    Security update.

--- tony_blog/settings.py
+++ tony_blog/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'c#togshqc9y=i^pto$5@87tk(k36n$vhh963wa!%x*2_(9x_)8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
