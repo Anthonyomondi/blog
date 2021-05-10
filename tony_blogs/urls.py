dd4889dae2fd0e0d76f4db573731ce16c91816c6
Anthonyomondi <otieno.anthonyomondi00@gmail.com>
2021-05-10 23:26:51 +0300

    mapped update view function to url

--- tony_blogs/urls.py
+++ tony_blogs/urls.py
@@ -2,7 +2,6 @@
 from django.urls import path
 
 from . import views
-from .views import delete_view
 
 app_name = 'tony_blogs'
 urlpatterns = [
@@ -14,5 +13,7 @@ urlpatterns = [
     path('blogs/<int:blog_id>/', views.blog, name='blog'),
     # Page for adding a new blog
     path('new_blog/', views.new_blog, name='new_blog'),
+    #page for delete view
     path('<int:blog_id>/delete_view', views.delete_view, name='delete'),
+    path('update_view/<int:blog_id>/', views.update_view, name='update' ),
 ]
