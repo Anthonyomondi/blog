a55034fefec1ccf2cd7b68c83721daa93a87d1f1
Anthonyomondi <otieno.anthonyomondi00@gmail.com>
2021-05-11 12:29:16 +0300

    added crispy forms
    add crispy forms to app and template pack

--- tony_blog/settings.py
+++ tony_blog/settings.py
@@ -49,6 +49,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #crispy forms
+    'crispy_forms',
+
     
 ]
 
@@ -131,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 #My settings
 LOGIN_URL = 'users:login'
