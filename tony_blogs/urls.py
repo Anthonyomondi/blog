586872cce7027b16666053e79f1374b71693a9a2
Tony-Otis <otieno.anthonyomondi00@gmail.com>
2021-05-12 10:59:29 +0300

    Update urls.py

--- tony_blogs/urls.py
+++ tony_blogs/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('blogs/<int:blog_id>/', views.blog, name='blog'),
     # Page for adding a new blog
     path('new_blog/', views.new_blog, name='new_blog'),
-    #page for delete view
+    # page for delete view
     path('<int:blog_id>/delete_view', views.delete_view, name='delete'),
-    path('update_view/<int:blog_id>/', views.update_view, name='update' ),
+    path('update_view/<int:blog_id>/', views.update_view, name='update'),
 ]
