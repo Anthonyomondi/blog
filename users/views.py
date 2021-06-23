4ac7bcc2506d70a7388c2a27cd124cf0766151ca
Tony-Otis <otieno.anthonyomondi00@gmail.com>
2021-06-23 22:43:52 +0300

    Update views.py
    code  refactore

--- users/views.py
+++ users/views.py
@@ -1,23 +1,41 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-
+from .forms import NewUserForm
 
 def register(request):
     """to register new user"""
     if request.method != 'POST':
         # Display a blank regstration form
-        form = UserCreationForm
+        form = NewUserForm
     else:
         # Process completed form.
-        form = UserCreationForm(data=request.POST)
+        form = NewUserForm(data=request.POST)
 
         if form.is_valid():
-            new_user = form.save()
+            user = form.save()
             # Log in user and redirect to home page
-            login(request, new_user)
+            login(request, user)
             return redirect('tony_blogs:index')
 
     # Display a blank/invalid form
     context = {'form': form}
     return render(request, 'registration/register.html', context)
+
+# def register(request):
+#     """to register new user"""
+#     if request.method != 'POST':
+#         # Display a blank regstration form
+#         form = UserCreationForm
+#     else:
+#         # Process completed form.
+#         form = UserCreationForm(data=request.POST)
+
+#         if form.is_valid():
+#             user = form.save()
+#             # Log in user and redirect to home page
+#             login(request, user)
+#             return redirect('tony_blogs:index')
+
+#     # Display a blank/invalid form
+#     context = {'form': form}
+#     return render(request, 'registration/register.html', context)
