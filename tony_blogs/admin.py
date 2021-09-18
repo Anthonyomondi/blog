021faca1385001d30ab8e2fe34a882482f330e8d
Tony-Otis <otieno.anthonyomondi00@gmail.com>
2021-09-18 10:57:00 +0300

    Update admin.py
    edit url path

--- tony_blogs/admin.py
+++ tony_blogs/admin.py
@@ -8,9 +8,9 @@ class BlogAdmin(admin.ModelAdmin):
     list_filter = ['date_created', 'updated_on']
     search_fields = ['title', 'content']
 
-class user_commentAdmin(admin.ModelAdmin):
-    list_display = ('author', 'content', 'date_created')
-    list_filter = ['date_created']
+# class user_commentAdmin(admin.ModelAdmin):
+#     list_display = ('author', 'content', 'date_created')
+#     list_filter = ['date_created']
 
 admin.site.register(Blog, BlogAdmin)
 admin.site.register(user_comment)
